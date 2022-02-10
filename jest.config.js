be5e2b39a3f3c5c260717976a86f13ad518b1833
Iain <iain@zora.co>
2022-02-10 15:31:24 -0500

    updates to indexer

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testRegex: "/test/.*\\.(test|spec)?\\.(ts|tsx)$",
+  testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'gql', 'graphql'],
   maxWorkers: 2,
-  modulePaths: ["<rootDir>/src"],
+  modulePaths: ['<rootDir>/src'],
+  setupFiles: ['dotenv/config'],
   transform: {
-    "^.+\\.(j|t)sx?$": ["ts-jest"],
-    "\\.(gql|graphql)$": ["@jagi/jest-transform-graphql"],
+    '^.+\\.(j|t)sx?$': ['ts-jest'],
+    '\\.(gql|graphql)$': ['@jagi/jest-transform-graphql'],
   },
 };
